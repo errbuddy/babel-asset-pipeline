11f133b5d47e248c9db560b4df4e70358101f25e
philipp <philipp@uberall.com>
2015-09-21 08:59:30 +0200

    changed to by default only process es6 files

--- src/main/groovy/asset/pipeline/babel/BabelProcessor.groovy
+++ src/main/groovy/asset/pipeline/babel/BabelProcessor.groovy
@@ -5,27 +5,37 @@ import asset.pipeline.AssetCompiler
 import asset.pipeline.AssetFile
 import asset.pipeline.AssetPipelineConfigHolder
 import com.google.gson.Gson
+import groovy.util.logging.Slf4j
 import org.mozilla.javascript.Context
 import org.mozilla.javascript.Scriptable
 import org.mozilla.javascript.ScriptableObject
 
+@Slf4j
 class BabelProcessor extends AbstractProcessor {
 
-    public static final ThreadLocal threadLocal = new ThreadLocal()
     static Scriptable globalScope
-    final String BABEL_OPTIONS = new Gson().toJson(AssetPipelineConfigHolder.config?.babel?.options ?: [:])
 
+    String babelOptions
+
+    public static final ThreadLocal THREAD_LOCAL = new ThreadLocal()
+    private static final CONVERTER = new Gson()
     private static boolean contextInitialized = false
 
     BabelProcessor(AssetCompiler precompiler) {
         super(precompiler)
-        ClassLoader classLoader = this.class.classLoader
-        if (!contextInitialized) {
+        if (config) {
+            babelOptions = CONVERTER.toJson(config.options ?: [:])
+        } else {
+            babelOptions = "{}" //
+        }
+        // only load all the javascript if it is enabled AND not yet initialized
+        if (enabled && !contextInitialized) {
+            ClassLoader classLoader = this.class.classLoader
             def shellJsResource = classLoader.getResource('asset/pipeline/babel/shell.js')
             def envRhinoJsResource = classLoader.getResource('asset/pipeline/babel/env.rhino.js')
             def objectResource = classLoader.getResource('asset/pipeline/babel/object.js')
 //            def requireResource = classLoader.getResource('asset/pipeline/babel/r.js')
-            def babelResource = classLoader.getResource('asset/pipeline/babel/babel.js')
+            def babelResource = classLoader.getResource('asset/pipeline/babel/browser.js')
             Context context = Context.enter()
             context.setOptimizationLevel(-1)
             context.setLanguageVersion(170)
@@ -41,24 +51,43 @@ class BabelProcessor extends AbstractProcessor {
     }
 
     String process(String input, AssetFile assetFile) {
-        try {
-            Context cx = Context.enter()
-            threadLocal.set(assetFile)
-            def compileScope = cx.newObject(globalScope)
-            compileScope.setParentScope(globalScope)
-            compileScope.put("es6Source", compileScope, input)
-            def result = cx.evaluateString(compileScope, "babel.transform(es6Source, $BABEL_OPTIONS).code", "babel command", 0, null)
-            return result.toString()
+        // only process if
+        // processing is enabled AND
+        // the given AssetFile is a Es6File OR processJsFiles is enabled
+        if (enabled && (processJsFiles || assetFile in Es6AssetFile)) {
+            try {
+                Context cx = Context.enter()
+                THREAD_LOCAL.set(assetFile)
+                def compileScope = cx.newObject(globalScope)
+                compileScope.setParentScope(globalScope)
+                compileScope.put("es6Source", compileScope, input)
+                def result = cx.evaluateString(compileScope, "babel.transform(es6Source, $babelOptions).code", "babel command", 0, null)
+                return result.toString()
 
-        } catch (Exception e) {
-            throw new Exception("babel-transforming $assetFile.name failed: $e")
-        } finally {
-            Context.exit()
+            } catch (Exception e) {
+                throw new Exception("babel-transforming $assetFile.name failed: $e")
+            } finally {
+                Context.exit()
+            }
+        } else {
+            return input
         }
     }
 
     static void print(text) {
-        println text
+        log.debug text
+    }
+
+    static def getConfig() {
+        AssetPipelineConfigHolder.config?.babel
+    }
+
+    static boolean isEnabled(){
+        config?.enabled != null ? config.enabled as Boolean : true
+    }
+
+    static boolean isProcessJsFiles(){
+        config?.processJsFiles != null ? config.processJsFiles as Boolean : false
     }
 
 }
--- src/test/groovy/asset/pipeline/babel/BabelProcessorSpec.groovy
+++ src/test/groovy/asset/pipeline/babel/BabelProcessorSpec.groovy
@@ -2,19 +2,65 @@ package asset.pipeline.babel
 
 import asset.pipeline.AssetCompiler
 import asset.pipeline.AssetPipelineConfigHolder
+import asset.pipeline.JsAssetFile
 import spock.lang.Specification
+import spock.lang.Stepwise
 import spock.lang.Unroll
 
 @Unroll
+@Stepwise
 class BabelProcessorSpec extends Specification {
 
-    def es6code = """
-        var a = ["Hydrogen","Helium","Lithium","Beryl­lium"];
-        var result = a.map(s => s.length);
-    """
+    private static final ES6_INPUT = """var a = ["Hydrogen","Helium","Lithium","Beryl­lium"];var result = a.map(s => s.length);"""
+
+    private static final ES5_OUTPUT = '''var a = ["Hydrogen", "Helium", "Lithium", "Beryl­lium"];var result = a.map(function (s) {
+  return s.length;
+});'''
+
+    private static final LOOSE_INPUT = "let foo = ['foo','bar']; for (var i of foo) {i};"
+    private static final ALL_LOOSE_OUT = '''var foo = ['foo', 'bar'];for (var _iterator = foo, _isArray = Array.isArray(_iterator), _i = 0, _iterator = _isArray ? _iterator : _iterator[Symbol.iterator]();;) {
+  var _ref;
+
+  if (_isArray) {
+    if (_i >= _iterator.length) break;
+    _ref = _iterator[_i++];
+  } else {
+    _i = _iterator.next();
+    if (_i.done) break;
+    _ref = _i.value;
+  }
+
+  var i = _ref;
+  i;
+};'''
+    private static final NON_LOOSE_OUT = '''var foo = ['foo', 'bar'];var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = foo[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    var i = _step.value;
+    i;
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator['return']) {
+      _iterator['return']();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}
+
+;'''
 
     def setup() {
-        AssetPipelineConfigHolder.config = [babel: [options: [blacklist: ['useStrict']]]]
+        AssetPipelineConfigHolder.config = [babel: [options: [blacklist: ['useStrict'], loose: 'all']]]
     }
 
     def "process should work"() {
@@ -23,18 +69,14 @@ class BabelProcessorSpec extends Specification {
         Es6AssetFile file = new Es6AssetFile()
         file.path = "test.js"
         when:
-        def result = processor.process(es6code, file)
+        def result = processor.process(ES6_INPUT, file)
         then:
-        result == '''
-var a = ["Hydrogen", "Helium", "Lithium", "Beryl­lium"];
-var result = a.map(function (s) {
-        return s.length;
-});'''
+        result == ES5_OUTPUT
     }
 
     def "process should work two times"() {
         given:
-        String code = new File("src/test/test.js").text
+        String code = new File("src/test/test.es6").text
         BabelProcessor processor = new BabelProcessor(new AssetCompiler())
         Es6AssetFile file = new Es6AssetFile()
         file.path = "test/test.js"
@@ -42,5 +84,60 @@ var result = a.map(function (s) {
         def result = processor.process(code, file)
         then:
         result != null // we just want to know that this works!
+    }
+
+    def "loose mode is working as expected if loose mode setting is #loose"() {
+        given:
+        AssetPipelineConfigHolder.config = [babel: [options: [blacklist: ['useStrict'], loose: loose]]]
+        and:
+        BabelProcessor processor = new BabelProcessor(new AssetCompiler())
+        and:
+        Es6AssetFile file = new Es6AssetFile()
+        file.path = "test.js"
+        when:
+        def result = processor.process(LOOSE_INPUT, file)
+        then:
+        result == out
+        where:
+        loose | out
+        'all' | ALL_LOOSE_OUT
+        null  | NON_LOOSE_OUT
+    }
+
+    def "process only works if enabled option is #enabled"() {
+        given:
+        AssetPipelineConfigHolder.config = [babel: [enabled: enabled, options: [blacklist: ['useStrict']]]]
+        and:
+        BabelProcessor processor = new BabelProcessor(new AssetCompiler())
+        and:
+        Es6AssetFile file = new Es6AssetFile()
+        file.path = "test.js"
+        when:
+        def processed = processor.process(ES6_INPUT, file)
+        def result = processed == ES5_OUTPUT
+        then:
+        isProcessed == result
+        where:
+        enabled | isProcessed
+        true    | true
+        false   | false
+    }
+
+    def "JsAssetFiles are only processed if enabled option  is #processJs"() {
+        given:
+        AssetPipelineConfigHolder.config = [babel: [processJsFiles: processJs, enabled: true, options: [blacklist: ['useStrict']]]]
+        BabelProcessor processor = new BabelProcessor(new AssetCompiler())
+        JsAssetFile file = new JsAssetFile()
+        file.path = "test.js"
+        when:
+        def processed = processor.process(ES6_INPUT, file)
+        def result = processed == ES5_OUTPUT
+        then:
+        isProcessed == result
+        where:
+        processJs | isProcessed
+        true      | true
+        false     | false
+
     }
 }
