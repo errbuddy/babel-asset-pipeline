f709bd8fc5589a923f9316de8489a7e37b222398
philipp <philipp@uberall.com>
2016-05-17 13:28:55 +0200

    fixed plugin descriptor

--- src/main/groovy/babel/asset/pipeline/BabelAssetPipelineGrailsPlugin.groovy
+++ src/main/groovy/babel/asset/pipeline/BabelAssetPipelineGrailsPlugin.groovy
@@ -4,66 +4,14 @@ import grails.plugins.*
 
 class BabelAssetPipelineGrailsPlugin extends Plugin {
 
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.1.4 > *"
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-        "grails-app/views/error.gsp"
-    ]
-
-    // TODO Fill in these fields
-    def title = "Babel Asset Pipeline" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
-    def description = '''\
-Brief summary/description of the plugin.
-'''
-
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/babel-asset-pipeline"
-
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-    // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
-
-    Closure doWithSpring() { {->
-            // TODO Implement runtime spring config (optional)
-        }
-    }
-
-    void doWithDynamicMethods() {
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    void doWithApplicationContext() {
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    void onChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    void onConfigChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    void onShutdown(Map<String, Object> event) {
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
+    def grailsVersion = "3.0.0 > *"
+    def title = "Babel Asset-Pipeline Plugin"
+    def author = "Philipp Eschenbach"
+    def authorEmail = "philipp@errbuddy.net"
+    def description = "Adds babel transformation to Asset-Pipeline."
+    def documentation = "https://github.com/errbuddy/babel-asset-pipeline"
+    def license = "APACHE"
+    def organization = [ name: "errbuddy", url: "https://errbuddy.net/" ]
+    def issueManagement = [ system: "GITHUB", url: "https://github.com/errbuddy/babel-asset-pipeline/issues" ]
+    def scm = [ url: "https://github.com/errbuddy/babel-asset-pipeline" ]
 }
